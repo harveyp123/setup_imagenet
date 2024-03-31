fb5206a813c46d09c519ac61edbf3180a3499bb9
xiexi1990 <xiexi_1990@126.com>
2024-03-31 03:50:28 +0000

    update setup_for_a6000.bash for vanilla full model teacher

--- setup_for_a6000.bash
+++ setup_for_a6000.bash
@@ -1,4 +1,6 @@
 ssh-copy-id -o ProxyJump=xix22010@137.99.0.102 xix22010@192.168.10.16
 cd ./HE_transfer_learning
-mkdir ./save_vanilla5_avg_shortcut_acc70
-scp -o ProxyJump=xix22010@137.99.0.102 xix22010@192.168.10.16:~/py_projects/HE_transfer_learning/save_vanilla5_avg_shortcut_acc70/deploy_vanilla5_shortcut_keepbn_acc70.pth ./save_vanilla5_avg_shortcut_acc70
+# mkdir ./save_vanilla5_avg_shortcut_acc70
+# scp -o ProxyJump=xix22010@137.99.0.102 xix22010@192.168.10.16:~/py_projects/HE_transfer_learning/save_vanilla5_avg_shortcut_acc70/deploy_vanilla5_shortcut_keepbn_acc70.pth ./save_vanilla5_avg_shortcut_acc70
+mkdir ./save_v5_avg_full_relu_shortcut_acc69
+scp -o ProxyJump=xix22010@137.99.0.102 xix22010@192.168.10.16:~/py_projects/HE_transfer_learning/save_v5_avg_full_relu_shortcut_acc69/full_v5_relu_shortcut_acc69.pth ./save_v5_avg_full_relu_shortcut_acc69
